3e4a2e3e318372355fd868f513f693691d47bd2a
Dmytro Hrynchyi <desevoker@gmail.com>
2023-11-04 00:12:51 +0200

    Change product page fadeIn style

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -30,7 +30,7 @@ export default function ProductPage() {
 			? (
 				<h1 style={{ textAlign: 'center'}}>Unable to load the product, try again later</h1>
 			) : (
-				<div className={`${style.page} ${grid.template} animate__animated animate__fadeInLeft`}>
+				<div className={`${style.page} ${grid.template} animate__animated animate__fadeInDownBig`}>
 					<BreadCrumbs category={category} productName={selectedProduct.name} />
 
 					<h1 className={`${style.page__title} ${style.title}`}>{selectedProduct.name}</h1>
